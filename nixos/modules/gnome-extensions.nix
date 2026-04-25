9040007640560182e06e3c36dc384601d9a3f4f8
Stas <stas@feather.xyz>
2026-04-25 16:37:58 -0400

    install: gnome-extension-manager

--- nixos/modules/gnome-extensions.nix
+++ nixos/modules/gnome-extensions.nix
@@ -1,10 +1,20 @@
+# vim: set tabstop=2 shiftwidth=2 expandtab:
+
 { pkgs, ... }:
 
-{
-  environment.systemPackages = with pkgs.gnomeExtensions; [
+let 
+  tools = with pkgs; [
+    gnome-extension-manager
+  ];
+
+  extensions = with pkgs.gnomeExtensions; [
     places-status-indicator
     clipboard-indicator
     extension-list
     top-bar-organizer
   ];
-}
+
+in 
+  {
+    environment.systemPackages = tools ++ extensions;
+  }
