5ffd4e6f755610883ec0143d819af149f601e7b5
Stas <stas@feather.xyz>
2026-04-25 00:57:24 -0400

    remove: duplicate bat package from wsl host (already in common.nix)

--- nixos/hosts/wsl/default.nix
+++ nixos/hosts/wsl/default.nix
@@ -1,13 +1,9 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   wsl.enable = true;
   wsl.defaultUser = "nixos";
   wsl.wslConf.interop.appendWindowsPath = false;
-
-  environment.systemPackages = with pkgs; [
-    bat
-  ];
 
   system.stateVersion = "25.11";
 }
