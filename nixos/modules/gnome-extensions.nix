232a909267619a0f0b8857c523fe2d077a752328
Stas <stas@feather.xyz>
2026-04-25 01:10:13 -0400

    remove: declarative extension locking so user can toggle freely via GUI

--- nixos/modules/gnome-extensions.nix
+++ nixos/modules/gnome-extensions.nix
@@ -1,26 +1,10 @@
 { pkgs, ... }:
 
-let
-  extensions = with pkgs.gnomeExtensions; [
+{
+  environment.systemPackages = with pkgs.gnomeExtensions; [
     places-status-indicator
     clipboard-indicator
     extension-list
     top-bar-organizer
   ];
-in
-{
-  environment.systemPackages = extensions;
-
-  services.desktopManager.gnome = {
-    extraGSettingsOverridePackages = [
-      pkgs.gnome-shell
-    ];
-
-    # NOTE: this override locks the enabled-extensions list — toggling extensions
-    # via the GNOME Extensions app won't persist across boots. Edit this list instead.
-    extraGSettingsOverrides = ''
-      [org.gnome.shell]
-      enabled-extensions=[${builtins.concatStringsSep "," (map (ext: "'${ext.extensionUuid}'") extensions)}]
-    '';
-  };
 }
