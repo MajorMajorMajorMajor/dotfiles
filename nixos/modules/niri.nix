803b8925d560af799ef59793284b9f6ca51f5269
Stas <stas@feather.xyz>
2026-04-25 23:58:40 -0400

    enable: super tap-to-overview via keyd (niri)

--- nixos/modules/niri.nix
+++ nixos/modules/niri.nix
@@ -21,6 +21,18 @@
       pkgs.xwayland-satellite
     ];
 
+  # GNOME-like Super behavior in niri:
+  # - tap left Super => send F24 (bound in niri config to overview)
+  # - hold left Super => keep acting as Super modifier
+  services.keyd = {
+    enable = true;
+    keyboards.default.settings = {
+      main = {
+        leftmeta = "overload(meta, f24)";
+      };
+    };
+  };
+
   # Lightweight Wayland login manager for a compositor-first setup.
   services.greetd = {
     enable = true;
