b5286ad088abde59afd7bdfa1b03308d2febeee7
Stas <stas@feather.xyz>
2026-04-25 22:18:52 -0400

    enable: xwayland-satellite (niri)

--- nixos/modules/niri.nix
+++ nixos/modules/niri.nix
@@ -7,32 +7,18 @@
     package = pkgs-unstable.niri;
   };
 
-  # niri itself doesn't manage Xwayland; run xwayland-satellite as a user service for X11 apps.
-  systemd.user.services.xwayland-satellite = {
-    description = "Xwayland outside your Wayland compositor";
-    bindsTo = [ "graphical-session.target" ];
-    partOf = [ "graphical-session.target" ];
-    after = [ "graphical-session.target" ];
-    requisite = [ "graphical-session.target" ];
-    wantedBy = [ "graphical-session.target" ];
-    serviceConfig = {
-      Type = "notify";
-      ExecStart = "${pkgs.xwayland-satellite}/bin/xwayland-satellite";
-      StandardOutput = "journal";
-      Restart = "on-failure";
-      RestartSec = "1s";
-    };
-  };
-
   programs.dank-material-shell = {
     enable = true;
     systemd.enable = true;
     dgop.package = pkgs-unstable.dgop;
   };
 
-  environment.systemPackages = 
-    with pkgs-unstable; [
+  environment.systemPackages =
+    (with pkgs-unstable; [
       fuzzel
+    ])
+    ++ [
+      pkgs.xwayland-satellite
     ];
 
   # Lightweight Wayland login manager for a compositor-first setup.
