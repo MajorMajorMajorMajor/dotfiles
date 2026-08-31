e9fd47eae4c68e6f8346d362e226917747c61709
Stas <stas@local>
2026-08-31 16:00:30 +0000

    Remove deprecated google support option in gvfs (GNOME 50 dropped support for it: https://itsfoss.com/news/gnome-drops-google-drive-support/

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -7,12 +7,6 @@
   services.desktopManager.gnome.enable = true;
   services.gnome.gnome-browser-connector.enable = true;
 
-  # Use unstable GVFS with Google backend enabled.
-  services.gvfs.package = pkgs-unstable.gvfs.override {
-    gnomeSupport = true;
-    googleSupport = true;
-  };
-
   # xwayland 24.1.10 (stable) has a tablet "slave" device regression that
   # breaks Krita pen input under Wayland. 24.1.11 (unstable) fixes it.
   # Override only mutter's xwayland input so the rest of the GNOME stack
